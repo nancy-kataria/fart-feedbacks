37fc6c0ef482c38d595efe1afd91ace6ece9d094
nancy-kataria <katarianancy8@gmail.com>
2025-01-13 23:55:18 -0800

    created feedback form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,35 @@ export default function Home() {
         </a>
       </h4>
 
-      <hr/>
+      <hr />
 
-      <footer></footer>
+      <form className="fart-section">
+        <div className="input-container">
+          <span className="fart-sparkle">
+            <label htmlFor="name">Name</label>
+          </span>
+
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="input-container">
+          <span className="fart-sparkle">
+            <label htmlFor="feedback">Feedback</label>
+          </span>
+          <textarea rows={5} id="feedback" name="feedback" required />
+        </div>
+
+        {/* <div>
+          <label>Give a sparkle if you liked Fartlabs </label>
+        </div> */}
+        {/* <p>You do not need to submit your real name.</p> */}
+        <div className="submit-container">
+        <button className="fart-button">Submit your farts</button>
+        </div>
+      </form>
+
+      <hr />
+
+      <footer>I made this because the FartLabs Founder called me a <b>Fake Fan</b></footer>
     </div>
   );
 }
