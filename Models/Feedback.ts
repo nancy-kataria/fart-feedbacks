77da486972b6c28d4f11a661a50a3196ed44b599
nancy-kataria <katarianancy8@gmail.com>
2025-01-14 16:45:49 -0800

    renamed Model name

---
+++ Models/Feedback.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const feedbackSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    feedback: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
+export default Feedback;
