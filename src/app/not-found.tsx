07352f18efc5debb8eb73d0f7faaea17bb4a762e
nancy-kataria <katarianancy8@gmail.com>
2025-01-15 16:28:09 -0800

    added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Custom404() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <h4>Sorry, the page you are looking for does not exist.</h4>
+    </div>
+  );
+}
