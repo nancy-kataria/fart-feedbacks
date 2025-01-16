80f7609092ae731f6e0968585745baee8d5f06f6
nancy-kataria <katarianancy8@gmail.com>
2025-01-15 22:32:32 -0800

    added headers to post request

--- src/app/api/SubmitFeedback/route.ts
+++ src/app/api/SubmitFeedback/route.ts
@@ -3,17 +3,32 @@ import connectDB from "../../../../lib/DBconnect";
 import Feedback from "../../../../Models/Feedback";
 
 export async function POST(request: NextRequest) {
-    await connectDB();
-    const data = await request.json();
-    
-    const feedbackRecord = new Feedback(data);
+  await connectDB();
+  const data = await request.json();
 
-    try{
-      await feedbackRecord.save()
-      return new Response(JSON.stringify({ message: 'Feedback Record created successfully' }), { status: 201 });
-    } catch(error){
-      console.log(error)
-      return new Response(JSON.stringify({ error: 'Error while creating feedback' }), { status: 500 });
-    }
+  const feedbackRecord = new Feedback(data);
+
+  try {
+    await feedbackRecord.save();
+    return new Response(
+      JSON.stringify({ message: "Feedback Record created successfully" }),
+      {
+        status: 201,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({ error: "Error while creating feedback" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
-
